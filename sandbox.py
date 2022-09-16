178ec3bffc0ef83f2d5adbc34f239d56a9f0bed0
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-09-16 17:45:31 +0900

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -9,6 +9,8 @@ If you do not know how to train a PyTorch model, it is too soon for you to dive 
 
 You're better off starting off with some simpler architectures, e.g. a simple feed forward network, in order to learn the basics
 """
+from torch import nn
+from torch.optim.lr_scheduler import StepLR
 
 import dataset as ds
 import utils
@@ -17,6 +19,7 @@ import torch
 import datetime
 import transformer_timeseries as tst
 import numpy as np
+import torch as th
 
 # Hyperparams
 test_size = 0.1
@@ -41,6 +44,7 @@ in_features_decoder_linear_layer = 2048
 max_seq_len = enc_seq_len
 batch_first = False
 
+cuda = "cuda"
 # Define input variables 
 exogenous_vars = [] # should contain strings. Each string must correspond to a column name
 input_variables = [target_col_name] + exogenous_vars
@@ -61,21 +65,28 @@ training_indices = utils.get_indices_entire_sequence(
     window_size=window_size, 
     step_size=step_size)
 
-# Making instance of custom dataset class
+trainint_datasets = training_data[input_variables].values
+# from sklearn.preprocessing import MinMaxScaler
+# scaler = MinMaxScaler(feature_range=(-1, 1))
+#
+#
+# trainint_datasets = scaler.fit_transform(trainint_datasets)
+
+cuda = "cuda"
 training_data = ds.TransformerDataset(
-    data=torch.tensor(training_data[input_variables].values).float(),
+    data=torch.tensor(trainint_datasets, device=cuda).float(),
     indices=training_indices,
     enc_seq_len=enc_seq_len,
     dec_seq_len=dec_seq_len,
     target_seq_len=output_sequence_length
     )
-
 # Making dataloader
-training_data = DataLoader(training_data, batch_size)
+
 
 i, batch = next(enumerate(training_data))
 
 src, trg, trg_y = batch
+print(src.shape)
 
 # Permute from shape [batch size, seq len, num features] to [seq len, batch size, num features]
 if batch_first == False:
@@ -84,8 +95,8 @@ if batch_first == False:
     src = src.permute(1, 0, 2)
     print("src shape changed from {} to {}".format(shape_before, src.shape))
 
-    shape_before = tgt.shape
-    tgt = tgt.permute(1, 0, 2)
+    shape_before = trg.shape
+    trg = trg.permute(1, 0, 2)
     print("src shape changed from {} to {}".format(shape_before, src.shape))
 
 model = tst.TimeSeriesTransformer(
@@ -109,9 +120,83 @@ tgt_mask = utils.generate_square_subsequent_mask(
     dim2=output_sequence_length
     )
 
-output = model(
-    src=src,
-    tgt=trg,
-    src_mask=src_mask,
-    tgt_mask=tgt_mask
-    )
+
+model = tst.TimeSeriesTransformer(
+    input_size=len(input_variables),
+    dec_seq_len=enc_seq_len,
+    out_seq_len =output_sequence_length,
+    num_predicted_features=1,
+    batch_first = True
+    ).to(cuda)
+
+# i, batch = next(enumerate(training_data))
+#
+# for batch in training_data:
+#     src, trg, trg_y = batch
+#     out = model.pred(src, trg)
+#     print(out)
+
+
+#
+#
+# #
+def th_fit(predictor, dataset, loss_func, metrc, optimizer, epoch, batch, validataion_split):
+    data_loader = DataLoader(dataset, batch_size=batch)
+    valid_size = (dataset.__len__()/batch * validataion_split)
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.9)
+
+
+    for step in range(epoch+1):
+        scheduler.step()
+        valid_set = []
+        train_set = []
+
+        predictor.train()
+        for i, t_set  in enumerate(data_loader):
+
+            src, trg, trg_y = t_set
+            if  len(valid_set) < valid_size :
+                valid_set.append(t_set)
+                continue
+
+            train_set.append(t_set)
+            hypothesis = predictor(src, trg, src_mask, tgt_mask)
+            loss = loss_func(hypothesis, trg_y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        predictor.eval()
+        with th.no_grad():
+            train = [(predictor(src, trg, src_mask, tgt_mask), trg_y) for src, trg, trg_y in train_set]
+            valid = [(predictor(src, trg, src_mask, tgt_mask), trg_y) for src, trg, trg_y in valid_set]
+            train_loss =np.mean([loss_func(xb, yb).cpu() for xb, yb in train])
+            train_metric = np.mean([metrc(xb, yb) for xb, yb in train])
+
+            valid_loss = np.mean([loss_func(xb, yb).cpu() for xb, yb in valid])
+            valid_metric = np.mean([metrc(xb, yb) for xb, yb in valid])
+            result = [ str(round(x, 6)) for x in [train_loss, train_metric, valid_loss, valid_metric]]
+
+            print("EPOCH {} : t_loss {}\tt_metrc {}\tval_loss {}\tval_metric {}".format(step, *result))
+
+
+    return predictor
+
+
+def ScaledRMSE(scaler):
+    def RMSE(yhat,y):
+        yhat = yhat[:,-1:]
+        y = y[:,-1:]
+        yhat = scaler.inverse_transform(yhat.cpu())
+        y = scaler.inverse_transform(y.cpu())
+        return np.sqrt(np.mean((yhat-y)**2))
+    return RMSE
+
+
+optimizer = th.optim.Adam(model.parameters(), lr=0.002)
+# loss_func = nn.BCELoss()
+loss_func = nn.MSELoss().to(cuda)
+max = 100
+validation_split = 0.1
+th_fit(model, training_data, loss_func, ScaledRMSE(scaler), optimizer, epoch=max, batch=256, validataion_split=0.1)
